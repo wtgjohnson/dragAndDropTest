e200051485b67da01bc89caf50aa211801cae9a7
wtgjohnson <wtgjohnson@gmail.com>
2024-01-28 14:48:55 -0800

    empty logic complete

--- TextureRect.gd
+++ TextureRect.gd
@@ -1,6 +1,7 @@
 extends TextureRect
 
-@export var dragged_from = false
+var dragged_from = false
+@export var empty = true
 
 func _notification(what):
 	if what == NOTIFICATION_DRAG_END:
@@ -8,27 +9,33 @@ func _notification(what):
 			dragged_from = false
 		elif dragged_from:
 			texture=ResourceLoader.load("res://small_Basic_red_dot.png")
+			empty=false
 
 func _get_drag_data(at_position):
-	
-	var preview_texture = TextureRect.new()
- 
-	preview_texture.texture = texture
-	preview_texture.expand_mode = 1
-	preview_texture.size = Vector2(30,30)
- 
-	var preview = Control.new()
-	preview.add_child(preview_texture)
- 
-	set_drag_preview(preview)
-	dragged_from=true
-	
-	texture=null
+	if not empty:
+		var preview_texture = TextureRect.new()
+	 
+		preview_texture.texture = texture
+		preview_texture.expand_mode = 1
+		preview_texture.size = Vector2(30,30)
+	 
+		var preview = Control.new()
+		preview.add_child(preview_texture)
+	 
+		set_drag_preview(preview)
+		dragged_from=true
+		empty=true
+		
+		texture=ResourceLoader.load("res://big_circle.svg")
 
-	return preview_texture.texture
+		return preview_texture.texture
  
 func _can_drop_data(_pos, data):
-	return data is Texture2D
+	return data is Texture2D and empty
  
 func _drop_data(_pos, data):
 	texture = data
+	empty=false
+	
+	
+
